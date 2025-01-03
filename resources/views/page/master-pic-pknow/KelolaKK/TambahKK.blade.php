69ca733f285f3e7efba52ffa87b8d9aa491491a4
mingmingang <saharromansah@gmail.com>
2025-01-03 14:12:49 +0700

    git push origin main

--- resources/views/page/master-pic-pknow/KelolaKK/TambahKK.blade.php
+++ resources/views/page/master-pic-pknow/KelolaKK/TambahKK.blade.php
@@ -167,4 +167,13 @@
 
 >>>>>>> 890bc16a8f979fb65c2f865433fa44430e1a37cc
 
+
+<script>
+     function sambalado(prodiId) {
+        if (prodiId) {
+            // Arahkan ke route dengan parameter prodiId
+            window.location.href = "{{ route('kelola_kk.create') }}?prodiId=" + prodiId +"&role=${encodeURIComponent(role)}&pengguna=${encodeURIComponent(name)}";
+        }
+    }
+    </script>
 @include('backbone.footer')
